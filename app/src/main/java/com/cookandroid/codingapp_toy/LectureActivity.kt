8fea41af922e58a21cd6415869ceb2439ea518b8
esak <lsls4868@gmail.com>
2022-04-06 21:53:05 +0900

    toy_app_Tablayout

--- app/src/main/java/com/cookandroid/codingapp_toy/LectureActivity.kt
+++ app/src/main/java/com/cookandroid/codingapp_toy/LectureActivity.kt
@@ -2,7 +2,11 @@ package com.cookandroid.codingapp_toy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_lecture.*
+import kotlinx.android.synthetic.main.custom_tab.view.*
 
 class LectureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +15,35 @@ class LectureActivity : AppCompatActivity() {
 
         val fragmentAdapter=FragmentAdapter(supportFragmentManager)
         list_viewpager.adapter=fragmentAdapter
+
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("AI")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("CSS")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("HTML")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("ID")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("JPG")))
+        tab_layout.addTab(tab_layout.newTab().setCustomView(createTabView("JS")))
+
+        list_viewpager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+
+        tab_layout.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if(tab!=null){
+                    list_viewpager.currentItem=tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+    }
+
+    private fun createTabView(tabName:String): View {
+        val tabView=LayoutInflater.from(baseContext).inflate(R.layout.custom_tab,null)
+        tabView.txt_name.text=tabName
+        return tabView
     }
 }
